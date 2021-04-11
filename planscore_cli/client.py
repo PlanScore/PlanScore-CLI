1c2e408eb9bd2f3c98d77428aead92c3b389b977
Michal Migurski <mike@teczno.com>
2021-04-10 20:32:02 -0700

    Completed planscore-client full loop

--- planscore_cli/client.py
+++ planscore_cli/client.py
@@ -1,2 +1,41 @@
+import time
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(description="Process some integers.")
+
+parser.add_argument("api_key", help="PlanScore.org API key")
+parser.add_argument("input_source", help="Path for input GeoJSON file")
+parser.add_argument("output_index", help="Path for output index JSON file")
+
+
 def main():
-    raise NotImplementedError()
+    args = parser.parse_args()
+
+    with open(args.input_source, "rb") as file1:
+        posted = requests.post(
+            "https://api.planscore.org/api-upload",
+            data=file1,
+            headers={
+                "Authorization": f"Bearer {args.api_key}",
+            },
+        )
+
+        index_url = posted.json()["index_url"]
+        plan_url = posted.json()["plan_url"]
+
+    start_time = time.time()
+
+    while True:
+        time.sleep(1)
+        got = requests.get(index_url)
+
+        if got.json()["status"] is not None:
+            break
+
+    elapsed = time.time() - start_time
+
+    with open(args.output_index, "w") as file2:
+        file2.write(got.text)
+
+    print(f"Wrote {plan_url} to {args.output_index} after {elapsed:.3f}sec")
--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 
 setup(
-    name = 'PlanScore-CLI',
-    url = 'https://github.com/PlanScore/PlanScore-CLI',
-    author = 'Michal Migurski',
-    description = '',
-    packages = [
-        'planscore_cli',
-        ],
-    test_suite = 'planscore.tests',
-    package_data = {
-        },
-    install_requires = [
-        ],
-    extras_require = {
-        },
-    entry_points = dict(
-        console_scripts = [
-            'planscore-client = planscore_cli.client:main',
-            ]
-        ),
+    name="PlanScore-CLI",
+    version="0.1.0",
+    url="https://github.com/PlanScore/PlanScore-CLI",
+    author="Michal Migurski",
+    description="",
+    packages=[
+        "planscore_cli",
+    ],
+    test_suite="planscore.tests",
+    package_data={},
+    install_requires=[
+        "requests==2.25.1",
+    ],
+    extras_require={},
+    entry_points=dict(
+        console_scripts=[
+            "planscore-client = planscore_cli.client:main",
+        ]
+    ),
 )
